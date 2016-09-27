9d44e3320d0163dc4ac3fb53222d507790f54f05
qurihara <qurihara@unryu.org>
2016-09-27 16:35:08 +0900

    ok

--- src/node/index.js
+++ src/node/index.js
@@ -1,6 +1,6 @@
 var MilkCocoa = require('milkcocoa');
-var milkcocoa = new MilkCocoa('xxxxx.mlkcca.com');
-var ds = milkcocoa.dataStore('xxx');
+var milkcocoa = new MilkCocoa('hotitl5z6gd.mlkcca.com');
+var ds = milkcocoa.dataStore('position');
 
 ds.on('send', function(sended) {
 //  console.log('sendされました！titleは'+sended.value.title+'contentは'+sended.value.value.index1);
